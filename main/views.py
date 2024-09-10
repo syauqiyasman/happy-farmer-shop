fbf0b693d905b51b2675e726691d8f20be4abea6
Syauqi Yasman <syauqiyasman@icloud.com>
2024-09-10 22:55:47 +0700

    Menambah produk

--- main/views.py
+++ main/views.py
@@ -4,11 +4,29 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name': 'Batang Singkong',
-        'price': '1000',
-        'description': 'Batang singkong berkualitas, tidak akan tumbuh menjadi jagung.',
-        'quantity': '10000',
-        'image': 'https://kabarpangan.com/wp-content/uploads/2020/08/23-bibit-IMG20200704141312-678x381.jpg',
+        'products': [
+            {
+                'name': 'Batang Singkong',
+                'price': '1000',
+                'description': 'Batang singkong berkualitas, tidak akan tumbuh menjadi jagung.',
+                'quantity': '10000',
+                'image': 'https://cdn.syauqiyasman.com/static/image/batang-singkong.webp',
+            },
+            {
+                'name': 'Susu Ikan',
+                'price': '7500',
+                'description': 'Susu ikan, dapat digunakan untuk memenuhi gizi anak bangsa.',
+                'quantity': '1000',
+                'image': 'https://cdn.syauqiyasman.com/static/image/susu-ikan.webp',
+            },
+            {
+                'name': 'Sawit Papua',
+                'price': '10000',
+                'description': 'Sawit Papua, produk ini berpotensi menyebabkan krisis iklim.',
+                'quantity': '100000',
+                'image': 'https://cdn.syauqiyasman.com/static/image/sawit-papua.webp',
+            },
+        ]
     }
 
     return render(request, "main.html", context)
